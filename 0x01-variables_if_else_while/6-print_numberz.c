3a16461b7a140afe04eb9ac4048190ed5e909383
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-06 23:40:18 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 10;
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
