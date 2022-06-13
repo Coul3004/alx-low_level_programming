2c2c3ec6d4c4da85fdb4d2e66f33022b8909d69a
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-13 13:50:48 +0000

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char ch;
+    for (ch = 'z'; ch >= 'a'; --ch)
+    {
+        putchar(ch);
+    }
+    printf("\n");
+    return (0);
+}
