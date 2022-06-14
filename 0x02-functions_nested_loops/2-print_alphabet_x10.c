46c3a36affe9d197ca6eb4ee30efcc624a50e49f
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-14 19:47:08 +0000

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,8 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
- * @ch: character
+ *
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
