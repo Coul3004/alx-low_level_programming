a634702379ff4b63724b64bfb6b4b4854aee4d49
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-14 19:44:31 +0000

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,13 +3,18 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
- * @ch: character 
+ * @ch: character
  */
 void print_alphabet_x10(void)
 {
 char ch;
+int i;
+for (i = 0; i < 10; i++)
+{
 for (ch = 'a'; ch <= 'z'; ch++)
 {
-_putchar((ch + '\n') * 10);
+_putchar(ch);
+}
+_putchar('\n');
 }
 }
