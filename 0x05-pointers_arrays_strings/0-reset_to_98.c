3464b3904ef04bda841a7cc2dc9cf95191f6f1ba
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-14 00:23:40 +0000

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and 
- * updates the value it points to to 98reset int value to 98
+ *updates the value it points to to 98reset int value to 98
  * @n : interger
  * Return: Always 0.
  */
