6eda5df74ee440482ef19cc61c75b9fd4cfb3065
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-15 02:28:44 +0000

    10. Great leaders are willing to sacrifice the numbers to save the people

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h> 
+
+/**
+ * is_numerical - check if it is a digit
+ * @n: number
+ * Return: 1 if is a number, else 0
+ */
+int is_numerical(unsigned int n)
+{
+return (n >= '0' &&  n <= '9');
+}
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: string
+ * Return: number
+ */
+int _atoi(char *s)
+{
+unsigned int number, i;
+int sign;
+sign = 1;
+number = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (is_numerical(s[i]))
+{
+number = (s[i] - 48) + number * 10;
+if (s[i + 1] == ' ')
+{
+break;
+}
+}
+else if (s[i] == '-')
+{
+sign *= -1;
+}
+}
+return (number * sign);
+}
