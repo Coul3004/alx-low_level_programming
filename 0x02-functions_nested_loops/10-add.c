9f6de1501a7fb39fdf395578c355a5674186894d
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-15 01:37:07 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - adds two integers and returns the result
+ * @i: integer #1
+ * @j: integer #2
+ * Return: Always 0.
+ */
+int add(int i, int j)
+{
+return (i + j);
+}
