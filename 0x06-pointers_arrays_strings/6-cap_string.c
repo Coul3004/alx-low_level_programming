3d42470fb77558f755c4ff0f2967f918a8c3e070
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-17 03:03:57 +0000

    6. Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,7 +8,6 @@
 char *cap_string(char *s)
 {
 int i;
-
 if (s[0] >= 'a' && s[0] <= 'z')
 {
 s[0] = s[0] - 32;
@@ -16,26 +15,26 @@ s[0] = s[0] - 32;
 i = 0;
 for (i = 0; s[0] != '\0'; i++)
 {
-    switch (s[i])
-    {
-    case ' ':
-    case '\t':
-    case '\n':
-    case ',':
-    case ';':
-    case '.':
-    case '!':
-    case '?':
-    case '\"':
-    case '(':
-    case ')':
-    case '{':
-    case '}':
-        if (s[i + 1] > 96 && s[i + 1] < 123)
-        {
-            s[i + 1] = s[i + 1] - 32;
-        }
-    }
+switch (s[i])
+{
+case ' ':
+case '\t':
+case '\n':
+case ',':
+case ';':
+case '.':
+case '!':
+case '?':
+case '\"':
+case '(':
+case ')':
+case '{':
+case '}':
+if (s[i + 1] > 96 && s[i + 1] < 123)
+{
+s[i + 1] = s[i + 1] - 32;
+}
+}
 }
 return (s);
 }
