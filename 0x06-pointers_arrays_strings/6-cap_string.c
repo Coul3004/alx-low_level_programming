91120d4bc253514f5d55ab3f8f4ad7e9d8e6ce59
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-17 03:10:02 +0000

    6. Expect the best

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,13 @@ if (s[0] >= 'a' && s[0] <= 'z')
 s[0] = s[0] - 32;
 }
 i = 0;
-for (i = 0; s[0] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
 switch (s[i])
 {
+case ' ':
+case '\t':
+case '\n':
 case ',':
 case ';':
 case '.':
@@ -27,9 +30,6 @@ case '(':
 case ')':
 case '{':
 case '}':
-case ' ':
-case '\t':
-case '\n':
 if (s[i + 1] > 96 && s[i + 1] < 123)
 {
 s[i + 1] = s[i + 1] - 32;
