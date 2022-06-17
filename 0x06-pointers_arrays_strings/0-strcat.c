85127f4fc91fc103836aa7149b5364c6ee312821
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-17 00:37:21 +0000

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,20 +8,19 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int x, y;
-
-    x = 0;
-    while (dest[x] = '\0')
-    {
-        x++;
-    }
-    y = 0;
-    while (src[y] = '\0')
-    {
-        dest[x] = src[y];
-        y++;
-        x++;
-    }
-    dest[x];
-    return (dest);    
+int x, y;
+x = 0;
+while (dest[x] = '\0')
+{
+x++;
+}
+y = 0;
+while (src[y] = '\0')
+{
+dest[x] = src[y];
+y++;
+x++;
+}
+dest[x] = '\0';
+return (dest);    
 }
