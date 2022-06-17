dabcc21ea7ed8f4aa0442087796295790862758c
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-17 00:35:16 +0000

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: string #1
+ * @dest: string #2
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+    int x, y;
+
+    x = 0;
+    while (dest[x] = '\0')
+    {
+        x++;
+    }
+    y = 0;
+    while (src[y] = '\0')
+    {
+        dest[x] = src[y];
+        y++;
+        x++;
+    }
+    dest[x];
+    return (dest);    
+}
