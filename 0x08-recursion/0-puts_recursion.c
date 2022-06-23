ae8f703e2f5644698f79aa7d1e0c7efb6bab681a
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-23 04:02:07 +0000

    0. She locked away a secret

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+    printf("%s\n", s);
+}
