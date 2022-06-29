4638855486b4d30f3a96f7bdb46366cbec3c47b3
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-29 02:15:16 +0000

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,25 +12,7 @@ int main(int argc, char *argv[])
     int add;
     int i;
     int error;
-/*
-    if (argc < 2)
-    {
-        printf("0\n");
-    }
-    else
-    {
-        for (i = 1; i < argc; i++)
-        {
-            if (*argv[i] >= '0' && *argv[i] <= '9')
-            {
-                add += atoi(argv[i]);
-            }
-            else
-            {
-                error = 1;
-            }
-        }
-*/
+
     if (argc > 1)
     {
         for (i = 1; i < argc; i++)
@@ -51,14 +33,15 @@ int main(int argc, char *argv[])
         return (0);
     }
     
-    if (error == 1)
+    if (error == 0)
+    {
+        printf("%d\n", add);
+        return (0);
+    }
+    else
     {
         printf("Error\n");
         return (1);
     }
-    else
-    {
-        printf("%d\n", add);
-    }
-    return (0);
+    
 }
