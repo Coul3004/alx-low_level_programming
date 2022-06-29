12dd6db6e8e871a6122a3756716c070706fc6059
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-29 02:11:22 +0000

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,36 +9,56 @@
  */
 int main(int argc, char *argv[])
 {
-int i;
-int add;
-int error;
-add = 0;
-if (argc < 2)
-{
-printf("%d\n", argc - 1);
-}
-else
-{
-for (i = 1; i < argc; i++)
-{
-if (*argv[i] >= '0' && *argv[i] <= '9')
-{
-add += atoi(argv[i]);
-}
-else
-{
-error = 1;
-}
-}
-if (error == 1)
-{
-printf("Error\n");
-return (1);
-}
-else
-{
-printf("%d\n", add);
-}
-}
-return (0);
+    int add;
+    int i;
+    int error;
+/*
+    if (argc < 2)
+    {
+        printf("0\n");
+    }
+    else
+    {
+        for (i = 1; i < argc; i++)
+        {
+            if (*argv[i] >= '0' && *argv[i] <= '9')
+            {
+                add += atoi(argv[i]);
+            }
+            else
+            {
+                error = 1;
+            }
+        }
+*/
+    if (argc > 1)
+    {
+        for (i = 1; i < argc; i++)
+        {
+            if (*argv[i] >= '0' && *argv[i] <= '9')
+            {
+                add += atoi(argv[i]);
+            }
+            else
+            {
+                error = 1;
+            }
+        }
+    }
+    else
+    {
+        printf("0\n");
+        return (0);
+    }
+    
+    if (error == 1)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    else
+    {
+        printf("%d\n", add);
+    }
+    return (0);
 }
