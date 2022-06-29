8f21cb62fbfbc227e9686ca760e66dea9811029a
Coul3004 <coulibalyfmohamed@gmail.com>
2022-06-29 02:34:05 +0000

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,47 +2,46 @@
 #include <stdlib.h>
 
 /**
- * main - adds positive numbers
- * @argc: argument count
- * @argv: argument vector
- * Return: 0 (Success) or 1 (Error).
- */
+* main - adds positive numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 (Success) or 1 (Error).
+*/
 int main(int argc, char *argv[])
 {
-    int i;
-    int add = 0;
-    int error = 0;
-    char errorText[] = "Error";
+int i;
+int add = 0;
+int error = 0;
+char errorText[] = "Error";
 
-    if (argc > 1)
-    {
-        for (i = 1; i < argc; i++)
-        {
-            if (*argv[i] >= '0' && *argv[i] <= '9')
-            {
-                add += atoi(argv[i]);
-            }
-            else
-            {
-                error = 1;
-                break;
-            }
-        }
-    }
-    else
-    {
-        printf("%d\n", argc - 1);
-        return (0);
-    }
-    
-    if (error == 0)
-    {
-        printf("%d\n", add);
-        return (0);
-    }
-    else
-    {
-        printf("%s\n", errorText);
-        return (1);
-    }
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+if (*argv[i] >= '0' && *argv[i] <= '9')
+{
+add += atoi(argv[i]);
+}
+else
+{
+error = 1;
+}
+}
+}
+else
+{
+printf("%d\n", argc - 1);
+return (0);
+}
+
+if (error == 0)
+{
+printf("%d\n", add);
+return (0);
+}
+else
+{
+printf("%s\n", errorText);
+return (1);
+}
 }
