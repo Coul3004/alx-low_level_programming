b13dbd7923d66c619c38c39a59675b490f4d88f5
Coul3004 <coulibalyfmohamed@gmail.com>
2022-07-05 15:26:00 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - define a new type with following elements
+*@name: string #1
+*@age: float
+*@owner: string #2
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
