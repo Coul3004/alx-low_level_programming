48d100b82e6194685120c84669c28bcb5d3ee86d
Coul3004 <coulibalyfmohamed@gmail.com>
2022-07-07 20:22:39 +0000

    1. A dog is the only thing on earth

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,11 @@
 #include "dog.h"
-
 /**
 * struct dog - initialize a variable of type
 * @d: pointer
 * @name: string #1
 * @age: float
 * @owner:string #2
+* Return: Nothing.
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
